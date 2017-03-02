c9de738598cec4dacb523ebaa9997c62fa285220
AIZAWA Hina <hina@bouhime.com>
2017-03-02 22:55:43 +0900

    init
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ src/MPlus1pFontAsset.php
@@ -0,0 +1,11 @@
+<?php
+namespace jp3cki\yii2\mplus1p;
+
+use yii\web\AssetBundle;
+
+class MPlus1pFontAsset extends AssetBundle
+{
+    public $css = [
+        'https://fonts.googleapis.com/earlyaccess/mplus1p.css',
+    ];
+}
